9ab4f2c390e33cac468da729c2c9eea74d4be977
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-21 06:51:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,8 @@ class ApplicationController < Sinatra::Base
     @song = Song.find_by_slug(params[:slug])
     @song.genres.clear
     genre = Genre.find_by_name(params[:song][:genre])
-
     @song.genres << genre
-    binding.pry
+  
     @song.update(params[:song])
 
     @song.save
