ee395b3d98082a9285befb7d89618e829b6ab095
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-21 07:29:00 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,21 +8,13 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/songs/:slug' do
-
     song = Song.find_by_slug(params[:slug])
-
     song.genres = []
     song.artist = Artist.find_or_create_by(:name => params[:artist_name])
     genre = Genre.find_by_name(params[:song][:genres])
     song.genres << genre
     song.save
-
-
-    
-
-
     redirect "songs/#{song.slug}"
-
   end
 
 end
