250d7787b18583acb14ab5898e821a2cc22f3140
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-21 07:20:14 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
     song = Song.find_by_slug(params[:slug])
 
     song.genres = []
-
+    song.artist = Artist.find_or_create_by(:name => params[:artist_name])
     song.save
 
 
